197d27f0d70e5ce6ddd08d97497a9a96e8bf30cc
aakbarkhan <aakbarkhan@gmail.com>
2022-03-31 19:41:57 +0530

    Add the routes and devise

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,10 @@
+class PublicRecipesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # get 'public_recipes/index'
+  # get 'public_recipes/show'
+  # get 'public_recipes/new'
+  # get 'recipe_foods/index'
+  # get 'recipe_foods/show'
+  # get 'recipe_foods/new'
+  # get 'recipes/index'
+  # get 'recipes/show'
+  # get 'recipes/new'
+  # get 'foods/index'
+  # get 'foods/show'
+  # get 'foods/new'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # root to: "home#index"
+  root "foods#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
